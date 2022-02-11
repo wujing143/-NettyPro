d4cbcbc5e904a652ca5287adcb4285b6ceaa1fe7
wujing143 <143520.wj>
2022-02-11 14:27:22 +0800

    Netty实现DubboRPC

--- src/main/java/com/jing/netty/protocoltcp/MyClient.java
+++ src/main/java/com/jing/netty/protocoltcp/MyClient.java
@@ -7,7 +7,15 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
-
+/**
+ * tcp粘包拆包问题解决方案
+ *
+ * 常用方案：使用自定义协议+编解码器来解决
+ * 关键就是要解决服务器端每次读取数据长度的问题，这个问题解决，就不会出现服务器多读或少读数据的问题，从而避免的 TCP 粘包、拆包。
+ * 看一个具体的实例：
+ * 要求客户端发送 5 个 Message 对象，客户端每次发送一个 Message 对象
+ * 服务器端每次接收一个 Message，分 5 次进行解码，每读取到一个 Message，会回复一个 Message 对象给客户端。
+ */
 public class MyClient {
     public static void main(String[] args)  throws  Exception{
 
--- src/main/java/com/jing/netty/source/echo2/EchoClient.java
+++ src/main/java/com/jing/netty/source/echo2/EchoClient.java
@@ -24,7 +24,7 @@ import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 
-/**
+/** 演示任务加入异步队列源码解析
  * Sends one message when a connection is open and echoes back any received
  * data to the server.  Simply put, the echo client initiates the ping-pong
  * traffic between the echo client and server by sending the first message to
